45d831dd71eb70636b312c8076571a84b6bd9222
olifitenkovlad11-lang <olifitenkovlad11@gmail.com>
2026-08-11 13:46:33 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = "imessage-demo-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png", "./avatar.jpeg"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
